6050ee6e0869651b49e4a3c19337b152e87520de
mary joseph <mary_jay@icloud.com>
2020-12-13 21:32:43 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [currentNumber, setCurrentNumber] = useState('');
+  const onNewNumber = (number) => {
+    console.log(number);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <div className='container'>
+          <h1>calculator</h1>
+          <div className='display'>{currentNumber}</div>
+          <div className='keyboard'>
+            <button className='key' onClick={() => onNewNumber('0')}>
+              0
+            </button>
+            <button className='key'>1</button>
+            <button className='key'>2</button>
+            <button className='key'>3</button>
+            <button className='key'>4</button>
+            <button className='key'>5</button>
+            <button className='key'>6</button>
+            <button className='key'>7</button>
+            <button className='key'>8</button>
+            <button className='key'>9</button>
+            <button className='key'>+</button>
+            <button className='key'>-</button>
+            <button className='key'>/</button>
+            <button className='key'>*</button>
+            <button className='key'>=</button>
+            <button className='key'>.</button>
+            <button className='key'>C</button>
+          </div>
+        </div>
       </header>
     </div>
   );
