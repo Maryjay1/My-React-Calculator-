3f62493de896c6fee4abd64648e643ece3fff967
mary joseph <mary_jay@icloud.com>
2020-12-14 22:29:12 +0000

    adding functionality to the number keys

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [currentNumber, setCurrentNumber] = useState('');
+  const [currentNumber, setCurrentNumber] = useState('0');
   const onNewNumber = (number) => {
-    console.log(number);
+    // when you press zero, you can only press it
+    // - when the currentNumber is larger than zero – tick!
+    // - or there's a decimal point
+    // const isAlreadyZero = number === '0' && currentNumber === '0';
+    // const hasADecimalPoint = currentNumber.includes('.'); // fill in lgic
+    // if (isAlreadyZero || !hasDecimalPoint) {
+    //   // Do nothing!
+    //   return;
+    // }
+
+    // if those conditions are met, continue
+
+    // first of all, add a number to the current string of numbers
+    const newNumber = currentNumber.concat(number);
+
+    // string concatenation!
+    // const newNumber2 = currentNumber + number;
+    // const newNumber3 = `${currentNumber}${number}`
+
+    // secondly, change currentNumber to reflect this new value
+    setCurrentNumber(newNumber);
   };
   return (
     <div className='App'>
@@ -16,22 +36,42 @@ function App() {
             <button className='key' onClick={() => onNewNumber('0')}>
               0
             </button>
-            <button className='key'>1</button>
-            <button className='key'>2</button>
-            <button className='key'>3</button>
-            <button className='key'>4</button>
-            <button className='key'>5</button>
-            <button className='key'>6</button>
-            <button className='key'>7</button>
-            <button className='key'>8</button>
-            <button className='key'>9</button>
+            <button className='key' onClick={() => onNewNumber('1')}>
+              1
+            </button>
+            <button className='key' onClick={() => onNewNumber('2')}>
+              2
+            </button>
+            <button className='key' onClick={() => onNewNumber('3')}>
+              3
+            </button>
+            <button className='key' onClick={() => onNewNumber('4')}>
+              4
+            </button>
+            <button className='key' onClick={() => onNewNumber('5')}>
+              5
+            </button>
+            <button className='key' onClick={() => onNewNumber('6')}>
+              6
+            </button>
+            <button className='key' onClick={() => onNewNumber('7')}>
+              7
+            </button>
+            <button className='key' onClick={() => onNewNumber('8')}>
+              8
+            </button>
+            <button className='key' onClick={() => onNewNumber('9')}>
+              9
+            </button>
             <button className='key'>+</button>
             <button className='key'>-</button>
             <button className='key'>/</button>
             <button className='key'>*</button>
             <button className='key'>=</button>
             <button className='key'>.</button>
-            <button className='key'>C</button>
+            <button className='key' onClick={() => setCurrentNumber('0')}>
+              C
+            </button>
           </div>
         </div>
       </header>
